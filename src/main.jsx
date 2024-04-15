c0628d560251c85ea79dd5b93021b6fc5cdb14c0
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-15 16:55:32 +0530

    Create browser router and setup root(/) and /login routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  redirect,
+} from "react-router-dom";
 import App from "./App.jsx";
+import ErrorPage from "./ErrorPage.jsx";
+import Login from "./components/login/Login.jsx";
+import loginAction from "./actions/loginAction.js";
+
+const baseUrl = "http://localhost:3000";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+        action: async ({ request }) => {
+          const formData = Object.fromEntries(
+            (await request.formData()).entries(),
+          );
+          const { successData, errorData } = await loginAction(
+            baseUrl,
+            formData,
+          );
+
+          if (errorData) {
+            return errorData.data;
+          }
+
+          return redirect(`/${successData.userId}`);
+        },
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
