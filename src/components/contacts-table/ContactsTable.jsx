24e6b02d5a9efb1d55b4e4844151f9c19b627be3
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-17 08:46:29 +0530

    Create initial ContactsTable component

---
+++ src/components/contacts-table/ContactsTable.jsx
@@ -0,0 +1,59 @@
+import PropTypes from "prop-types";
+import { Form, useNavigate, useLocation } from "react-router-dom";
+
+const ContactsTable = ({ contacts }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  function handleEdit(contactId) {
+    navigate(`${location.pathname}/${contactId}/edit`);
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Full name</th>
+          <th>Gender</th>
+          <th>Email</th>
+          <th>Phone number</th>
+          <th></th>
+        </tr>
+      </thead>
+      {contacts && (
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr key={contact._id}>
+                <td>{`${contact.firstName} ${contact.lastName}`}</td>
+                <td>{contact.gender}</td>
+                <td>{contact.email}</td>
+                <td>{contact.phoneNumber}</td>
+                <td>
+                  <button type="button" onClick={() => handleEdit(contact._id)}>
+                    Edit
+                  </button>
+                  <Form method="delete">
+                    <input
+                      type="hidden"
+                      name="contact_id"
+                      value={contact._id}
+                    />
+                    <button type="submit">Delete</button>
+                  </Form>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      )}
+    </table>
+  );
+};
+
+ContactsTable.propTypes = {
+  contacts: PropTypes.array,
+  onEdit: PropTypes.func,
+};
+
+export default ContactsTable;
