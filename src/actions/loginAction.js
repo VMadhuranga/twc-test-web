1830a82c6c66167692dd721755a7f68f8d2dfc52
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-17 08:51:06 +0530

    Update axios config to send credentials

--- src/actions/loginAction.js
+++ src/actions/loginAction.js
@@ -1,9 +1,12 @@
 import axios from "axios";
-import {} from "react-router-dom";
 
 const loginAction = async (baseUrl, formData) => {
   try {
-    const successData = (await axios.post(`${baseUrl}/login`, formData)).data;
+    const successData = (
+      await axios.post(`${baseUrl}/login`, formData, {
+        withCredentials: true,
+      })
+    ).data;
     return { successData };
   } catch (error) {
     const errorData = error.response.data;
