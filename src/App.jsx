d303b2877b9b1855c68e30b8e3eaeabb5c3d9cdf
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-17 12:25:20 +0530

    Add Logout link

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link, useMatch } from "react-router-dom";
 
 const App = () => {
+  const loginPath = useMatch("/login");
+  const registerPath = useMatch("/register");
+
   return (
     <>
       <header>
         <h1>Contacts Portal</h1>
+        {!(loginPath || registerPath) && <Link to={"/logout"}>Logout</Link>}
       </header>
       <main>
         <Outlet />
