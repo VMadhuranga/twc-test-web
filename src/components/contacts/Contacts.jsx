c8ee0c1ce06b5cbcc4eda20adb568a5ffb33b96e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-18 17:51:51 +0530

    Add Contacts component styles

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,5 +1,7 @@
 import { useLoaderData, useNavigate, useLocation } from "react-router-dom";
 import ContactsTable from "../contacts-table/ContactsTable";
+import styles from "./Contacts.module.css";
+import { buttonPrimary } from "../../App.module.css";
 
 const Contacts = () => {
   const contacts = useLoaderData();
@@ -11,28 +13,38 @@ const Contacts = () => {
   }
 
   return (
-    <section>
+    <>
       {contacts.length > 0 ? (
-        <>
+        <section className={styles.contactsSectionWithContacts}>
           <h2>Contacts</h2>
-          <button type="button" onClick={handleAddNewContact}>
+          <button
+            type="button"
+            className={buttonPrimary}
+            onClick={handleAddNewContact}
+            data-add-new
+          >
             Add new contact
           </button>
           <ContactsTable contacts={contacts} />
-        </>
+        </section>
       ) : (
-        <>
+        <section className={styles.contactsSectionWithoutContacts}>
           <h2>Welcome,</h2>
           <p>
             This is where your contacts live. Click the button below to add new
             contact.
           </p>
-          <button type="button" onClick={handleAddNewContact}>
+          <button
+            type="button"
+            className={buttonPrimary}
+            onClick={handleAddNewContact}
+            data-add-new
+          >
             Add new contact
           </button>
-        </>
+        </section>
       )}
-    </section>
+    </>
   );
 };
 
