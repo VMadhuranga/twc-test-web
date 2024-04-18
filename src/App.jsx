6214b0c37afaf7464675bddfb3a2377f88b6b7d5
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-18 18:31:43 +0530

    Add loading indicator

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Outlet, Link, useMatch } from "react-router-dom";
+import { Outlet, Link, useMatch, useNavigation } from "react-router-dom";
 import "./App.module.css";
 
 const App = () => {
   const loginPath = useMatch("/login");
   const registerPath = useMatch("/register");
+  const navigation = useNavigation();
 
   return (
     <>
@@ -12,7 +13,11 @@ const App = () => {
         {!(loginPath || registerPath) && <Link to={"/logout"}>Logout</Link>}
       </header>
       <main>
-        <Outlet />
+        {navigation.state === "loading" || navigation.state === "loading" ? (
+          <p style={{ textAlign: "center" }}>Loading....</p>
+        ) : (
+          <Outlet />
+        )}
       </main>
     </>
   );
